189494fee23237b27836aad260989cd918c4260b
code-guru-007 <py.guru.007@gmail.com>
2025-02-06 03:34:46 +0900

    init commit

--- src/data.js
+++
@@ -1,9 +0,0 @@
-import csv from 'csvtojson';
-
-async function fetchDeals() {
-  const csvFilePath = 'test.csv'; // Replace with the actual path to your CSV file
-  const jsonArray = await csv.fromFile(csvFilePath);
-  return jsonArray;
-}
-
-export { fetchDeals };
